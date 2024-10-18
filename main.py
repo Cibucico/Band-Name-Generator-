1ebd8a34985bb9c93f487bdffa70c184661419e2
Sibusiso Skhosana <103737109+Cibucico@users.noreply.github.com>
2024-10-19 00:42:55 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+ #1. A greeting message from my program.
+print("Welcome to the band name generator.")
+
+
+#2. Ask the user for the city that they grew up in. 
+city = input("Which City did you grow up in?\n")
+
+
+#3. Ask the user for the name of a pet
+pet = input("What is the name of your pet?\n")
+
+
+#4. Combine the name of the city and pet.
+print("Your Band name could be Bristol Rabbit " + city + " " + pet)
+
+
+#5. Make sure the input cursor shows on a new line.
